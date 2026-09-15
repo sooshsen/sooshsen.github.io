3d674617ab90a3d8810e8459937c219671d75e6c
Sushmita Senapati <26025679+sooshsen@users.noreply.github.com>
2026-09-16 01:39:53 +0200

    fixed content

--- script.js
+++ script.js
@@ -24,10 +24,56 @@ toggle.addEventListener("change", function() {
 
 
 
+// welcome title in diff languages
+const welcome = document.getElementById("welcome");
+
+const greetings = [
+    "Welcome",
+    "Willkommen",
+    "Bienvenue",
+    "Bienvenido",
+    "Benvenuto",
+    "Welkom",
+    "स्वागत है",
+    "স্বাগতম",
+    "ようこそ",
+    "환영합니다",
+    "欢迎"
+];
+
+let currentGreeting = 0;
+
+setInterval(function() {
+    welcome.style.opacity = 0;
+
+    setTimeout(function() {
+        currentGreeting++;
+
+        if (currentGreeting >= greetings.length) {
+            currentGreeting = 0;
+        }
+
+        welcome.textContent = greetings[currentGreeting];
+        welcome.style.opacity = 1;
+    }, 1250);
+
+}, 5000);
+
+
+
+// Know-me slider functionality
 let information;
 
 const slider = document.getElementById("info-slider");
 const introduction = document.getElementById("introduction");
+
+const skillsPanel = document.getElementById("skills-panel");
+const researchPanel = document.getElementById("research-panel");
+
+const skillsList = document.getElementById("skills-list");
+const researchList = document.getElementById("research-list");
+
+const cvPanel = document.getElementById('cv-panel');
 
 fetch("data/details_2026.json")
     .then(response => {
@@ -59,6 +105,7 @@ function updateIntroduction() {
         introduction.appendChild(p);
     });
 
+    // link to CV
     if (info.cv) {
         const p = document.createElement("p");
         p.className = "personal-details";
@@ -70,6 +117,44 @@ function updateIntroduction() {
 
         p.appendChild(link);
         introduction.appendChild(p);
+    }
+
+    // skills panel
+    skillsList.innerHTML = "";
+
+    if (info.skills) {
+        info.skills.forEach(skill => {
+            const li = document.createElement("li");
+            li.textContent = skill;
+            skillsList.appendChild(li);
+        });
+
+        skillsPanel.classList.add("show");
+    } else {
+        skillsPanel.classList.remove("show");
+    }
+
+    // research panel
+    researchList.innerHTML = "";
+
+    if (info.research) {
+        info.research.forEach(item => {
+            const li = document.createElement("li");
+            li.textContent = item;
+            researchList.appendChild(li);
+        });
+
+        researchPanel.classList.add("show");
+    } else {
+        researchPanel.classList.remove("show");
+    }
+
+    // CV panel
+
+    if (info.cv && Number(level) >= 11) {
+        cvPanel.classList.add("show");
+    } else {
+        cvPanel.classList.remove("show");
     }
 }
 
